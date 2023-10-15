fedec67f4824d7bbf0504fd11d8c3f65b4565b0b
Lívia Nascimento <liviapnd@gmail.com>
2023-10-15 19:15:50 -0300

    component: big card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-big-card',
@@ -9,6 +10,10 @@ export class BigCardComponent implements OnInit {
 
   constructor() { }
 
+  type: string = "Artigo";
+  subject: string = "Boas práticas";
+  title: string = "Como evitar um código Frankenstein"
+  imgSource: string = "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
   ngOnInit(): void {
   }
 
