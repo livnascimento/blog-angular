f25244951499f02f39a183ec4c7e450e9db0b949
Lívia Nascimento <liviapnd@gmail.com>
2023-10-21 08:35:53 -0300

    chore: adicionando o home.module ao app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { TitleComponent } from './components/title/title.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
-import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent,
-    TitleComponent,
-    BigCardComponent,
-    SmallCardComponent,
-    HomeComponent
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
