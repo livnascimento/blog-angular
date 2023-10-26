446a6a71e04dd41c65fe14cb8fd8aa89177a62cc
Lívia Nascimento <liviapnd@gmail.com>
2023-10-26 16:02:54 -0300

    fix: correções nas variáveis de ambiente

--- src/app/services/axios-config.ts
+++ src/app/services/axios-config.ts
@@ -1,4 +1,4 @@
-import { environment } from '../../environments/environments';
+import { environment } from '../../environments/environment';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
